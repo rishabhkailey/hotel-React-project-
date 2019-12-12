a65769b8c1e5ab012fced713998fad30945dd548
rishabhkailey <rishabhveersinghkailay@gmail.com>
2019-12-12 22:37:46 +0530

    minor changes

--- src/api_calls/getCarouselImages.js
+++ src/api_calls/getCarouselImages.js
@@ -1,15 +1,11 @@
 let images = [
     {
-        src: 'https://doc-04-08-docs.googleusercontent.com/docs/securesc/leaahl62bp4lfrk9oh4rgcb9a7l8khb1/fshskhsmtqtuk8t46dj6s00dv9ah3umn/1576058400000/04400094518470609959/04400094518470609959/1tA003ns82ij5LXd7w79cvT_SF9GBzbs4?e=download&authuser=0&nonce=t3hnnkarsa0s4&user=04400094518470609959&hash=mg5mjr1evnn5',
-        label: 'easy and fast booking'
+        src: 'https://www.familyvacationcritic.com/uploads/sites/19/2018/09/large-multi-gen-travel-1280x640.jpg',
+        label: 'enjoy your next trip with your family'
     },
     {
-        src: 'https://doc-0g-08-docs.googleusercontent.com/docs/securesc/leaahl62bp4lfrk9oh4rgcb9a7l8khb1/nt33kslohfg2qrbf2g2i8apfjpa2cs6p/1576058400000/04400094518470609959/04400094518470609959/1HD7Yzh-eeGOhuXvpYtfMimwcuE-EmMm2?e=download&authuser=0',
+        src: 'http://static.asiawebdirect.com/m/phuket/portals/kosamui-com/homepage/samui-top10s/top10-local-thai-restaurants-in-bophut-beach/pagePropertiesImage/10-Best-Local-Thai-Restaurants-in-Bophut-Beach.jpg',
         label: 'hotels from all around the world'
-    },
-    {
-        src: 'https://doc-0o-08-docs.googleusercontent.com/docs/securesc/leaahl62bp4lfrk9oh4rgcb9a7l8khb1/or21vvdnvs95ev78gb9vj5lr54qjiljk/1576058400000/04400094518470609959/04400094518470609959/1vPcl8qU_f6I2DYxZc1RnuEsc7T8mDmHM?e=download&authuser=0.jpg',
-        label: 'luxury hotels'
     }
 ]
 export default images;
